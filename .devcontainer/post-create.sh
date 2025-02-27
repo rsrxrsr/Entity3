d3177a9e0730a228b7de289d808d3da6a679fd82
rsrxrsr <36836051+rsrxrsr@users.noreply.github.com>
2025-02-27 13:22:26 +0000

    Runing

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -14,10 +14,11 @@ mvn -v
 # Crear usuario y base de datos
 psql -U postgres -c "CREATE DATABASE entity3db;"
 psql -U postgres -c "CREATE ROLE sa WITH LOGIN PASSWORD 's3cr3t0';"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON SCHEMA public TO sa;"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO sa;"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO sa;"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO sa;"
+psql -U postgres -c "ALTER ROLE sa WITH SUPERUSER;"
+#psql -U postgres -c "GRANT ALL PRIVILEGES ON SCHEMA public TO sa;"
+#psql -U postgres -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO sa;"
+#psql -U postgres -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO sa;"
+#psql -U postgres -c "GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO sa;"
 
 echo "* * Spring BOOT * *"
 
